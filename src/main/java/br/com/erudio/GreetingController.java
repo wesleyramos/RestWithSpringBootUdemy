86ee03b2a8fa47b479b550dbb98317623b989094
Wesley <wesley.goncalves@furukawaelectric.com>
2022-01-13 20:55:56 -0300

    Hello world

---
+++ src/main/java/br/com/erudio/GreetingController.java
@@ -0,0 +1,20 @@
+package br.com.erudio;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+@RestController
+public class GreetingController {
+
+    private static final String template = "Hello, %s!";
+    private final AtomicLong count = new AtomicLong();
+
+    @RequestMapping("/greeting")
+    public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+        return new Greeting(count.incrementAndGet(), String.format(template, name));
+    }
+
+}
